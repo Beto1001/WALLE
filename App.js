62bf020fc86f9d32b498b11c71af80ec77c792a5
Jerrisaurio <brandon.villegas012003@gmail.com>
2024-02-16 22:07:20 -0600

    Implementando modificando app.js y moviendo el navigation a un archivo de texto en walle ubicado en features

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
 import { StyleSheet} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import NotificationsScreen from './features/notifications/application/screens/NotificationsScreen';
-import HomeScreen from './HomeScreen';
+import AuthScreen from './features/auth/application/screens/AuthScreen'
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-          <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'Welcome'}}
-         />
-          <Stack.Screen
-          name="Notifications"
-          component={NotificationsScreen}
-          options={{title: 'Notifications'}}
-         />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthScreen />
   );
 }
 const styles = StyleSheet.create({
